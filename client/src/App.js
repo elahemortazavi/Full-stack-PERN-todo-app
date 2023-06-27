a717cd9d673760f6d1385b528f82779025fe3acb
elahemortazavi <emor5680@gmail.com>
2023-06-27 16:58:52 +0100

    client side , InputTodo.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+
+//import components:
+import InputTodo from "./components/InputTodo";
+// import ListTodos from "./components/ListTodos";
 
 function App() {
   return (
-   <div>hello</div>
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        {/* <ListTodos /> */}
+      </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
+  //This code snippet is handling form submission by sending an HTTP POST request to a specified URL with the provided data in the request body. After a successful submission, it redirects the user to the home page.
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Pern Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
