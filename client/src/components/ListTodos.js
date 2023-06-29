d5b2e72ee77dbf5e1ea06cbf9498f9d12e5cb951
elahemortazavi <emor5680@gmail.com>
2023-06-29 15:48:01 +0100

    EditTodo.js

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -6,7 +6,7 @@ const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
   //delete todo function
-
+  //Implement the "deleteTodo" function. This function is responsible for deleting a todo item from the server. It makes a DELETE request to the specified URL using the "fetch" function. If the request is successful, it updates the "todos" state by removing the deleted todo from the list.
   const deleteTodo = async (id) => {
     try {
       const deleteTodo = await fetch(`http://localhost:5001/todos/${id}`, {
@@ -19,6 +19,7 @@ const ListTodos = () => {
     }
   };
 
+  //Implement the "getTodos" function. This function retrieves the list of todos from the server by making a GET request using the "fetch" function. It then updates the "todos" state with the received data:
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5001/todos");
@@ -29,7 +30,7 @@ const ListTodos = () => {
       console.error(err.message);
     }
   };
-
+  //call the "getTodos" function when the component mounts:
   useEffect(() => {
     getTodos();
   }, []);
