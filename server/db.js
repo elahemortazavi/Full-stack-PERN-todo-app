a4773a599c408df09d5d65ef5d00f3219412369d
elahemortazavi <emor5680@gmail.com>
2023-07-15 19:37:34 +0100

    pool changed

--- server/db.js
+++ server/db.js
@@ -11,15 +11,15 @@
 // module.exports = pool;
 
 
-const { Pool } = require("pg");
-require("dotenv").config();
+// const { Pool } = require("pg");
+// require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_DATABASE,
+// });
 
-module.exports = pool;
+// module.exports = pool;
